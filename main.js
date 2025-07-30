7dd28c205d5f3376bf6df3c09d17829e1f3026cd
Ismatullo Mukhamedzhanov <mukhamejanov.ismat@gmail.com>
2025-07-30 16:01:43 +0500

    Last changes made. Need to revise by teamlead.

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+const state = 0;
+
 function nextPage(){
+    const state = 1;
+
     const pageone = document.getElementById("firstPage");
     pageone.style.display="none";
 
     const pagetwo = document.getElementById("secondPage");
-    pagetwo.style.display="block";
+    pagetwo.style.display="flex";
+}
 
-    
+if (state == 0){
+    pageone.style.display = "flex";
+    pagetwo.style.display = "none";
+}
+else{
+    pageone.style.display = "none";
+    pagetwo.style.display = "flex";
 }
