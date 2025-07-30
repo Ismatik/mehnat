e23e83193d1eb3a00551cb6614b15cf42003c84f
Ismatullo Mukhamedzhanov <mukhamejanov.ismat@gmail.com>
2025-07-30 14:44:16 +0500

    Commited changes to fit the sizes  same

---
+++ main.js
@@ -0,0 +1,9 @@
+function nextPage(){
+    const pageone = document.getElementById("firstPage");
+    pageone.style.display="none";
+
+    const pagetwo = document.getElementById("secondPage");
+    pagetwo.style.display="block";
+
+    
+}
